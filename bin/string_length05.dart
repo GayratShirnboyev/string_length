1e00b237ac624e2fe96579f17b3548d7ba54400e
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-04 10:30:12 +0500

    main5

--- bin/string_length05.dart
+++ bin/string_length05.dart
@@ -7,6 +7,14 @@
     Returns:
         total length of strings
 */
+String func(s1, s2) {
+  String f = "x + s";
+  String x = ('s1.length');
+  String s = ('s2.length');
+  return (s1 + s2);
+}
+
+// write your code here
 void main() {
-  // write your code here
+  print(func('hello', 'salom'));
 }
